8829eab9544735e46d965542171188fd8ce400de
Taishi_MainPC\taishi <reolch@gmail.com>
2023-02-18 02:29:23 +0900

    [Fix] グローバルStateを使用するように修正した。

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,20 @@
+import { useContext } from "react";
 import { useState } from "react";
+
+import { AdminFlagContext } from "./components/providers/AdminFlagProvider";
 import { Card } from "./components/Card";
 
-export default function App() {
-  const [isAdmin, setIsAdmin] = useState(false);
+export function App() {
+  const {isAdmin, setIsAdmin} = useContext(AdminFlagContext);
 
-  const onClickSwitch = () => {
+  const handledOnClick = () => {
     setIsAdmin(!isAdmin);
-  };
+  }
 
   return (
     <div>
       {isAdmin ? <span>管理者です</span> : <span>管理者以外です</span>}
-      <button onClick={onClickSwitch}>切り替え</button>
+      <button onClick={handledOnClick}>切り替え</button>
       <Card isAdmin={isAdmin} />
     </div>
   );
--- app/src/components/EditButton.jsx
+++ app/src/components/EditButton.jsx
@@ -1,13 +1,17 @@
+import { useContext } from "react";
+import { AdminFlagContext } from "./providers/AdminFlagProvider";
+
 const style = {
     width: "100px",
     padding: "6px",
     borderRadius: "8px"
   };
-  
-  export const EditButton = (props) => {
-    const { isAdmin } = props;
+
+  export const EditButton = () => {
+    const { isAdmin } = useContext(AdminFlagContext);
+    console.log(isAdmin);
     return (
-      <button style={style} disabled={!isAdmin}>
+      <button style={style} disabled={!isAdmin} >
         編集
       </button>
     );
